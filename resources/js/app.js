0f11bf78fb339d7d8da4faac0748df503a66ebb9
JaveedYara72 <javeedymd.iitg@gmail.com>
2021-02-18 15:07:30 +0530

    commit on 'master'

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@
 //aur yaha pe compile hone ke baad (we compile using tools),
 // wo jaake public ka js folder mein save hoga and from there, we include them into the index.js
 
+console.log("hello from app js")
