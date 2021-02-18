0420674758c51d75b383a059d78e11d125289491
JaveedYara72 <javeedymd.iitg@gmail.com>
2021-02-18 14:50:09 +0530

    commit on 'master'

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss','public/css/app.css');
+
+//the first parameter is the folder where we have the js files, the second parameter is where we want the files to be compiled
