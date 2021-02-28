47f58016721f4daa4163f57514f60444948d72a8
JaveedYara72 <javeedymd.iitg@gmail.com>
2021-02-28 14:08:58 +0530

    seond commit

--- server.js
+++ server.js
@@ -14,15 +14,30 @@ const PORT = process.env.PORT || 3000 // process.env is a variable that is outsi
 //Assets
 app.use(express.static('public')) // .static is a middle ware, 'public' is also the folder
 
-app.get('/',(req,res)=>{
-    // res.send("Hello from server") -> this will be used when there is no rendering involved
-    res.render('home')
-})
+
 
 // Time to set the template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    // res.send("Hello from server") -> this will be used when there is no rendering involved
+    res.render('home')
+})
+
+//routes
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
